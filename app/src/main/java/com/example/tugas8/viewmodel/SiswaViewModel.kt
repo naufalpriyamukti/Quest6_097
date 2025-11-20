0f80b0b50fbdd552de38b9a415ecaf20b86a1e44
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-20 12:51:14 +0700

    Menambahkan MutableStateFlow untuk mengelola siswa

--- app/src/main/java/com/example/tugas8/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/tugas8/viewmodel/SiswaViewModel.kt
@@ -1,8 +1,13 @@
 package com.example.tugas8.viewmodel
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.StateFlow
 
 class SiswaViewModel : ViewModel() {
-    private
+    private val_statusUI = MutableStateFlow(value = Siswa())
+
+    val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
+
+
 
 }
