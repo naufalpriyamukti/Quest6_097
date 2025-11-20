4455aee407844687ca7d99404a546ff18089741c
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-20 13:14:41 +0700

    Menambahkan selectable modifier untuk RadioButton berdasarkan pilihan gender

--- app/src/main/java/com/example/tugas8/view/FormSiswa.kt
+++ app/src/main/java/com/example/tugas8/view/FormSiswa.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -80,12 +81,19 @@ fun FormSiswa(
                 .width(250.dp), thickness = Thickness,color =
                 Color.Red)
             Row{
-                pilihanJK.forEach {
-                        item->
-                    Row(verticalAlignment = Alignment.CenterVertically){
+                pilihanJK.forEach { item->
+                    Row(modifier = Modifier.selectable(
+                        selected = txtGender == item,
+                        onClick = {
+                            txtGender = item
+                        }
+                    ),
+                        verticalAlignment = Alignment.CenterVertically){
                         RadioButton(
-                            selected = false,
-                            onClick = {item}
+                            selected = txtGender == item,
+                            onClick = {
+                                txtGender = item
+                            }
                         )
                     }
                 }
@@ -107,7 +115,7 @@ fun FormSiswa(
             Spacer(modifier = Modifier.height(30.dp))
             Button(
                 modifier = Modifier.fillMaxWidth(1f),
-                onClick = OnSubmitBtnClick
+                onClick = OnSubmitButtonClicked
             ){
                 Text(stringResource(id = R.string.submit))
             }
