0a54bcba335a9d5d110166074a709d0f2e94b219
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-20 13:08:34 +0700

    Memperbarui value dengan varibel txtNama dan fungsi onValueChange untuk memperbarui nama

--- app/src/main/java/com/example/tugas8/view/FormSiswa.kt
+++ app/src/main/java/com/example/tugas8/view/FormSiswa.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -64,13 +65,15 @@ fun FormSiswa(
             horizontalAlignment = Alignment.CenterHorizontally)
         {
             OutlinedTextField(
-                value = "",
+                value = txtNama,
                 singleLine = true,
                 modifier = Modifier
                     .padding(top = 20.dp)
                     .width(250.dp),
                 label = {Text(text = "Nama Lengkap")},
-                onValueChange = {}
+                onValueChange = {
+                    txtNama = it
+                }
             )
             HorizontalDivider(modifier = Modifier
                 .padding(20.dp)
