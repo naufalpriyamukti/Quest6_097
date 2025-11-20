080ee9f281f9aaf6cc6799a2ed01821225f118d0
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-20 12:51:50 +0700

    Menambahkan fungsi setSiswa untuk memperbarui informasi siswa

--- app/src/main/java/com/example/tugas8/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/tugas8/viewmodel/SiswaViewModel.kt
@@ -8,6 +8,14 @@ class SiswaViewModel : ViewModel() {
 
     val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
 
-
+    fun setSiswa(ls:MutableList<String>) {
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(
+                nama = ls[0],
+                gender = ls[1],
+                alamat = ls[2]
+            )
+        }
+    }
 
 }
