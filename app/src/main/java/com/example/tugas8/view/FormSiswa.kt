606b50d1afdd139daca39444abae4113f54cc109
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-20 13:05:19 +0700

    Menambahkan variabel state untuk nama, alamat, dan gender

--- app/src/main/java/com/example/tugas8/view/FormSiswa.kt
+++ app/src/main/java/com/example/tugas8/view/FormSiswa.kt
@@ -22,6 +22,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -37,6 +41,15 @@ fun FormSiswa(
     onSubmitButtonClicked: (MutableList<String>) -> Unit,
     modifier: Modifier = Modifier
 ){
+    var txtNama by rememberSaveable { mutableStateOf("") }
+    var txtAlamat by remember { mutableStateOf("") }
+    var txtGender by remember { mutableStateOf("") }
+    val listData: MutableList<String> = mutableListOf(
+        txtNama,
+        txtGender,
+        txtAlamat
+    )
+
     Scaffold (modifier= Modifier,
         {
             TopAppBar(
